286add76a778d56870de4abc712c259c85f66c5d
michaelweir14 <weirm@mail.gvsu.edu>
2018-10-10 23:57:13 -0400

    spinners working properly

--- app/src/main/java/edu/gvsu/cis/conversioncalculatorapp/SettingsActivity.java
+++ app/src/main/java/edu/gvsu/cis/conversioncalculatorapp/SettingsActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 
@@ -20,22 +22,33 @@ public class SettingsActivity extends AppCompatActivity {
         ((ActionBar) actionBar).setDisplayHomeAsUpEnabled(true);
 
 
-        
+
         Spinner toSpinner  = (Spinner) findViewById(R.id.toSpinner);
-        Spinner fromSpinner = (Spinner) findViewById(R.id.toSpinner);
+        Spinner fromSpinner = (Spinner) findViewById(R.id.fromSpinner);
+        String[] lengthSpinner = new String[]{
+          "Meters","Yards","Miles"
+        };
+        String[] lengthSpinner2 = new String[]{
+                "Yards","Meters","Miles"
+        };
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, lengthSpinner2);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, lengthSpinner);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        toSpinner.setAdapter(adapter2);
+        fromSpinner.setAdapter(adapter);
 
 
+        toSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
 
-        toSpinner.setOnClickListener((View v)->{
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
 
-            //ToSpinner method
-        });
-
-        fromSpinner.setOnClickListener((View v)->{
-
-
-            //fromSpinner method
+            }
         });
 
     }
